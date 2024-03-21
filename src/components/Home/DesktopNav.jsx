417694bb30e999e5f8d7c40e58bb758a6b4cce9e
Gia Duong <giaduong5906@gmail.com>
2024-03-21 07:05:37 -0400

    nav, mediaqueries checked

--- src/components/Home/DesktopNav.jsx
+++ src/components/Home/DesktopNav.jsx
@@ -1,5 +1,26 @@
 export default function DesktopNav() {
-    return(
-        
-    )
+  return (
+    <nav id="desktop-nav">
+      <div class="logo">Gia Duong</div>
+      <div>
+        <ul class="nav-links">
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#funfact">Fun Fact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
--- src/components/Home/HomeScreen/index.jsx
+++ src/components/Home/HomeScreen/index.jsx
@@ -1,8 +1,12 @@
 import HeroSection from "../HeroSection";
+import DesktopNav from "../DesktopNav";
+import HamburgerNav from "../HamburgerNav";
 
 export default function Home() {
   return (
     <>
+      <DesktopNav></DesktopNav>
+      <HamburgerNav></HamburgerNav>
       <HeroSection></HeroSection>
     </>
   );
